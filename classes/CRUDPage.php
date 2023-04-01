ac2d0fa65bb6e4280bb41aa0d65e21fba559c40b
Fylyb <78358886+Fylyb@users.noreply.github.com>
2023-04-01 20:38:18 +0200

    Add files via upload

---
+++ classes/CRUDPage.php
@@ -0,0 +1,31 @@
+<?php
+
+abstract class CRUDPage extends BasePage
+{
+    public const STATE_FORM_REQUESTED = 0;
+    public const STATE_DATA_SENT = 1;
+
+    public const ACTION_INSERT = "insert";
+    public const ACTION_UPDATE = "update";
+    public const ACTION_DELETE = "delete";
+
+    protected function redirect(string $action, bool $success) : void
+    {
+        $data = [
+            'action' => $action,
+            'success' => $success ? 1 : 0
+        ];
+        header('Location: /room/list.php?' . http_build_query($data) );
+        exit;
+    }
+    protected function redirectStaff(string $action, bool $success) : void
+    {
+        $data = [
+            'action' => $action,
+            'success' => $success ? 1 : 0
+        ];
+        header('Location: /staff/list.php?' . http_build_query($data) );
+        exit;
+    }
+
+}
