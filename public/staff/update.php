a2dd45831a478ace6c3fd4678fc8d23752f3ae23
Fylyb <78358886+Fylyb@users.noreply.github.com>
2023-04-23 20:28:14 +0200

    Update update.php

--- public/staff/update.php
+++ public/staff/update.php
@@ -30,7 +30,7 @@ class EmployeeUpdatePage extends CRUDPage
             if (!$this->employee)
                 throw new NotFoundException();
 
-            $stmt=PDOProvider::get()->query("SELECT `name` roomName, room_id roomId FROM room");
+            $stmt=PDOProvider::get()->query("SELECT `name` roomName, room_id roomId, `no` FROM room");
             $this->keys=$stmt->fetchAll();
         }
 
@@ -43,6 +43,62 @@ class EmployeeUpdatePage extends CRUDPage
             $this->errors = [];
             $this->keys = filter_input(INPUT_POST, 'keys', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
             $isOk = $this->employee->validate($this->errors);
+            if (!$isOk)
+            {
+                $this->state = self::STATE_FORM_REQUESTED;
+            }
+            else
+            {
+                $success = $this->employee->update();
+                if($success){
+
+                $stmt = PDOProvider::get()->prepare("DELETE FROM `key` WHERE `key`.employee = :employeeId");
+                $stmt->execute(['employeeId' => $this->employee->employee_id]);
+                    foreach($this->keys as $key) {
+                        $stmt = PDOProvider::get()->prepare("INSERT INTO `key` (employee, room) VALUES (:employeeId, :roomId)");
+                        $success = $stmt->execute(['employeeId' => $this->employee->employee_id, 'roomId' => $key]);
+                        if(!$success)
+                            break;
+                    }
+                }
+
+
+                //ulož je
+                //$success = $this->room->update();
+
+
+                //přesměruj
+                $this->redirectStaff(self::ACTION_UPDATE, $success);
+            }
+        }
+    }
+
+    protected function pageBody()
+    {
+        return MustacheProvider::get()->render(
+            'employeeForm',
+            [
+                'employee' => $this->employee,
+                'errors' => $this->errors,
+                'keys' => $this->keys
+            ]
+        );
+    }
+
+    private function findState() : void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
+            $this->state = self::STATE_DATA_SENT;
+        else
+            $this->state = self::STATE_FORM_REQUESTED;
+    }
+
+}
+
+$page = new EmployeeUpdatePage();
+$page->render();
+
+?>
             if (!$isOk)
             {
                 $this->state = self::STATE_FORM_REQUESTED;
